698f518de5331f115b40ddd5e6ba333ad5486b66
Joe Morris <joemorris810@gmail.com>
2018-02-12 15:31:54 +0000

    Fixed bootstrap/jquery links and
    adding button collapse

--- site/site.php
+++ site/site.php
@@ -11,10 +11,16 @@
 
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Cardiff University LC</title>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script> 
-    <link href="../libs/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
     <link href="../css/styling.css" rel="stylesheet">
     <script src="../js/script.js"></script>
     <link rel="shortcut icon" href="../favicon.ico" />
@@ -138,14 +144,15 @@
         </div>
       </div>
 
-
-
     <div id="live-chat"> <!-- Start chat bot integration -->
-      <iframe
-          width="300"
-          height="400"
-          src="https://console.dialogflow.com/api-client/demo/embedded/2df684ab-3175-4cd8-9bd4-d0f6ecfa5bac">
-      </iframe>
+      <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#bot">Personal Tutorbot</button>
+      <div id="bot" class="collapse">
+        <iframe
+            width="300"
+            height="400"
+            src="https://console.dialogflow.com/api-client/demo/embedded/2df684ab-3175-4cd8-9bd4-d0f6ecfa5bac">
+        </iframe>
+      </div>
     </div> <!-- end live-chat integration -->
 
     <!-- cardiff footer -->
